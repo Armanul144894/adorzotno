5d3aab156a43d152b5c0ed52e21f05dff47501dc
Armanul144894 <armanul15-2842@diu.edu.bd>
2026-03-12 13:00:23 +0600

    done

--- src/app/components/SideBar.js
+++ src/app/components/SideBar.js
@@ -10,16 +10,11 @@ export default function SideBar() {
     return (
         <div className='lg:sticky top-24 rounded-2xl max-h-[90vh] overflow-x-auto hidden lg:block'>
             <aside
-                className={`-translate-x-full lg:translate-x-0 lg:relative inset-y-0 lg:bg-transparent z-40 w-64 lg:w-72 `}
+                className={`-translate-x-full lg:translate-x-0 lg:relative inset-y-0 lg:bg-transparent z-40 w-60 `}
             >
                 <div className="bg-gray-50 rounded-lg shadow-md p-4 lg:sticky lg:top-24">
-                    <div className="flex items-center justify-between mb-4 lg:hidden">
-                        <h2 className="text-xl font-bold text-gray-800">Categories</h2>
-                        {/* <button onClick={() => setSidebarOpen(false)}>
-                            <X size={24} />
-                        </button> */}
-                    </div>
-                    <h2 className="hidden lg:block text-xl font-bold text-gray-800 mb-4">Categories</h2>
+                    
+                    <h2 className="text-xl font-semibold text-gray-800 mb-4">Categories</h2>
                     <div className="space-y-2">
                         {categories.map((category, index) => (
                             <Link
@@ -28,16 +23,16 @@ export default function SideBar() {
                                             .replace(/&/g, 'and')
                                             .replace(/[^a-z0-9]+/g, '-')
                                             .replace(/(^-|-$)/g, '')}`}
-                                className="w-full cursor-pointer flex items-center justify-between py-2 hover:bg-teal-50 rounded-lg transition-colors group"
+                                className="w-full cursor-pointer flex items-center justify-between py-1 hover:bg-teal-50 rounded-lg transition-colors group"
                             >
                                 <div className="flex items-center gap-3">
-                                    <div className='w-8 mx-auto'><span className="text-2xl">{category.icon}</span></div>
-                                    <span className="text-gray-700 group-hover:text-primary font-semibold">
+                                    <div className='w-8 mx-auto'><span className="text-md">{category.icon}</span></div>
+                                    <span className="text-gray-700 group-hover:text-primary text-sm ">
                                         {category.name}
                                     </span>
                                 </div>
-                                <span className="text-xs px-2 py-1 rounded">
-                                    <ChevronRight/>
+                                <span className="text-sm">
+                                    <ChevronRight size={16}/>
                                 </span>
                             </Link>
                         ))}
