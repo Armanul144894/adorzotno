a0bf28341083066fed9139c0a6d33cbfa266bd42
FahimAhmmed <fahimahmmed44@gmail.com>
2026-04-17 15:07:57 +0600

    header search

--- src/components/layout/HeaderSearch.js
+++ src/components/layout/HeaderSearch.js
@@ -1,66 +1,16 @@
 "use client";
-import { useEffect, useRef, useState } from "react";
-import { Search, ChevronDown } from "lucide-react";
+import { Search } from "lucide-react";
 
 export default function HeaderSearch() {
-  const [selectedCategory, setSelectedCategory] = useState("All");
-  const [open, setOpen] = useState(false);
-  const dropdownRef = useRef(null);
-
-  const categories = ["All", "Medicine", "Healthcare", "Lab Test", "Beauty"];
-
-  useEffect(() => {
-    const handleClickOutside = (e) => {
-      if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
-        setOpen(false);
-      }
-    };
-
-    document.addEventListener("mousedown", handleClickOutside);
-    document.addEventListener("touchstart", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-      document.removeEventListener("touchstart", handleClickOutside);
-    };
-  }, []);
-
   return (
     <>
-      <div className="flex flex-1 max-w-2xl" ref={dropdownRef}>
+      <div className="flex flex-1 max-w-2xl">
         <div className="flex w-full relative">
-          {/* Category Dropdown */}
-          <div className="relative">
-            <button
-              onClick={() => setOpen(!open)}
-              className="flex items-center gap-1 px-4 py-4 shadow-sm bg-blue-50 border-2 border-red-600/40 rounded-l-lg text-sm font-medium"
-            >
-              {selectedCategory}
-              <ChevronDown size={16} />
-            </button>
-
-            {open && (
-              <ul className="absolute left-0 top-full z-50 w-40 bg-white border shadow-md">
-                {categories.map((cat) => (
-                  <li
-                    key={cat}
-                    onClick={() => {
-                      setSelectedCategory(cat);
-                      setOpen(false);
-                    }}
-                    className="px-3 py-2 hover:bg-gray-100 cursor-pointer text-sm"
-                  >
-                    {cat}
-                  </li>
-                ))}
-              </ul>
-            )}
-          </div>
-
           {/* Search Input */}
           <input
             type="text"
             placeholder="Search for products..."
-            className="w-full px-4 py-3 shadow-sm bg-blue-100 border-2 border-x border-l-0  border-primary/40 focus:outline-none"
+            className="w-full px-4 py-3 shadow-sm bg-blue-100 border-2 border-primary/40 rounded-l-lg focus:outline-none"
           />
 
           {/* Search Button */}
