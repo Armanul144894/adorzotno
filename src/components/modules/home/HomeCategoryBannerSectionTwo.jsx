76b0f9798b2e620a225eb48ba02543e741f36be5
FahimAhmmed <fahimahmmed44@gmail.com>
2026-06-02 15:26:44 +0600

    banner

--- src/components/modules/home/HomeCategoryBannerSectionTwo.jsx
+++ src/components/modules/home/HomeCategoryBannerSectionTwo.jsx
@@ -2,52 +2,76 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useMemo } from "react";
+import { getImageUrl } from "@/lib/imageHelpers";
+import { useGetBannersQuery } from "@/redux/features/banner/bannerApi";
 
-const banners = [
-  {
-    id: 4,
-    image: "/images/banner/banner55.jpg",
-    title: "Special Offers",
-    link: "#",
-  },
-  {
-    id: 5,
-    image: "/images/banner/banner11.jpg",
-    title: "New Arrivals",
-    link: "#",
-  },
-  {
-    id: 6,
-    image: "/images/banner/banner33.jpg",
-    title: "Wellness Picks",
-    link: "#",
-  },
-];
-
-const BannerImg = ({ banner, sizes = "50vw" }) => (
-  <Link
-    href={banner.link}
-    className="group block overflow-hidden rounded-xl"
-  >
-    <Image
-      src={banner.image}
-      alt={banner.title}
-      width={0}
-      height={0}
-      sizes={sizes}
-      className="h-full w-full object-fill transition-transform duration-500 group-hover:scale-105"
-    />
-  </Link>
-);
+const BannerImg = ({ banner, sizes = "50vw", className = "" }) => {
+  return (
+    <Link
+      href={banner.link}
+      className={`group relative block h-full overflow-hidden rounded-xl ${className}`}
+    >
+      <Image
+        src={banner.image}
+        alt={banner.title}
+        fill
+        sizes={sizes}
+        className="object-fill transition-transform duration-500 group-hover:scale-105"
+        unoptimized
+      />
+    </Link>
+  );
+};
 
 export default function HomeCategoryBannerSectionTwo() {
+  const { data: banners = [] } = useGetBannersQuery();
+
+  const sectionBanners = useMemo(() => {
+    const filteredBanners = banners.filter(
+      (banner) =>
+        banner?.status === "active" &&
+        banner?.banner_type === "homepage_middle_2",
+    );
+
+    const toBannerItem = (banner, fallbackId) =>
+    ({
+      id: banner?.id || fallbackId,
+      image: getImageUrl(banner?.image_path),
+      title: banner?.title || "Homepage banner",
+      link: banner?.banner_url || "#",
+    });
+
+    return {
+      large: toBannerItem(
+        filteredBanners.find((banner) => banner?.position === "large"),
+        "homepage-middle-2-large",
+      ),
+      smallTop: toBannerItem(
+        filteredBanners.find((banner) => banner?.position === "small_top"),
+        "homepage-middle-2-small-top",
+      ),
+      smallBottom: toBannerItem(
+        filteredBanners.find((banner) => banner?.position === "small_bottom"),
+        "homepage-middle-2-small-bottom",
+      ),
+    };
+  }, [banners]);
+
   return (
-    <div className="mb-8 grid gap-3" style={{ gridTemplateColumns: "2fr 1fr" }}>
-      <BannerImg banner={banners[2]} sizes="50vw" />
-      <div className="flex flex-col gap-3">
-        <BannerImg banner={banners[0]} sizes="25vw" />
-        <BannerImg banner={banners[1]} sizes="25vw" />
+    <div
+      className="mb-8 grid h-[250px] grid-rows-[1.6fr_1fr] gap-3 sm:h-[340px] md:h-[420px] lg:h-[380px] lg:grid-cols-[2fr_1fr] lg:grid-rows-1 xl:h-[420px]"
+    >
+      <div className="order-1 h-full min-h-0 overflow-hidden rounded-xl">
+        <BannerImg banner={sectionBanners.large} sizes="50vw" className="h-full" />
+      </div>
+      <div className="order-2 grid h-full min-h-0 grid-cols-2 gap-3 lg:grid-cols-1 lg:grid-rows-2">
+        <div className="min-h-0">
+          <BannerImg banner={sectionBanners.smallTop} sizes="25vw" className="h-full" />
+        </div>
+        <div className="min-h-0">
+          <BannerImg banner={sectionBanners.smallBottom} sizes="25vw" className="h-full" />
+        </div>
       </div>
     </div>
   );
